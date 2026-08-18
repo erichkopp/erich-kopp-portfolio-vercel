fa49f8579da79d9204ba475d2ceeae2b221c872c
Erich Kopp <erichkopp@gmail.com>
2026-08-18 12:58:47 -0400

    Add more portfolio projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,8 @@ import { FaGithub, FaChevronRight, FaChevronLeft } from "react-icons/fa";
 import photo from "../images/photo610.png";
 import ecommerce from "../images/ecommerce610.png";
 import slippery from "../images/slippery610.png";
+import theorum from "../images/theorum610.png";
+import wildcoast from "../images/wildcoast610.png";
 
 export default function Portfolio() {
   const [cardNumber, setCardNumber] = useState(0)
@@ -11,14 +13,14 @@ export default function Portfolio() {
   const handleCardArrows = (e) => {
     if (e.target.id === "left") {
       if (cardNumber === 0) {
-        setCardNumber(2)
+        setCardNumber(4)
       } else {
         setCardNumber(cardNumber - 1)
       }
     }
 
     if (e.target.id === "right") {
-      if (cardNumber === 2) {
+      if (cardNumber === 4) {
         setCardNumber(0)
       } else {
         setCardNumber(cardNumber + 1)
@@ -60,6 +62,26 @@ export default function Portfolio() {
             siteLink="https://shop.erichkopp.com/"
           />
       }
+      {
+        cardNumber === 3 &&
+          <PortfolioCard
+            title="Theorum Wheel"
+            description="A tiny music theory app that displays chord theory in a visual way. One of the the first projects I ever made, I still personally use it frequently as a quick music theory reference."
+            image={theorum}
+            githubLink="https://github.com/erichkopp/ChordApp"
+            siteLink="https://erichkopp.github.io/TheorumWheel"
+          />
+      }
+      {
+        cardNumber === 4 &&
+          <PortfolioCard
+            title="WildCoast Digital Studio"
+            description="A landing page for my freelance web development business specializing in custom-coded projects, WordPress, and e-commerce solutions."
+            image={wildcoast}
+            githubLink=""
+            siteLink="https://www.wildcoastdigitalstudio.com/"
+          />
+      }
 
 
 
@@ -67,7 +89,7 @@ export default function Portfolio() {
         <div className="card-arrows">
           <span id="left" onClick={handleCardArrows}><FaChevronLeft />&nbsp; Previous</span>
 
-          <div id="card-indicators">{[0,1,2].map((num) => (
+          <div id="card-indicators">{[0,1,2,3,4].map((num) => (
             <span key={num} onClick={() => setCardNumber(num)}>
               <p style={{ opacity: cardNumber === num ? 1 : 0.3 }}>&bull;</p>
             </span>
@@ -93,10 +115,12 @@ function PortfolioCard({ title, description, image, githubLink, siteLink }) {
         <div className="description">{description}</div>
 
         <div className="portfolio-buttons">
-          <a href={githubLink} target="_blank" rel="noreferrer">
-            <FaGithub />
-            <span>GitHub</span>
-          </a>
+          { githubLink && (
+            <a href={githubLink} target="_blank" rel="noreferrer">
+              <FaGithub />
+              <span>GitHub</span>
+            </a>
+          )}
           <a href={siteLink} target="_blank" rel="noreferrer">
             <span>Visit Site</span>
           </a>
